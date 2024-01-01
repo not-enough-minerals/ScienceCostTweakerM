392bc41c401633ffe7243faba161e9b2740eb18b
Carlin Mische <carlinmische@gmail.com>
2023-12-31 22:55:05 -0600

    Update ScienceCostTweaker_1.01.02.lua (#95)
    * Update ScienceCostTweaker_1.01.02.lua
    
    Fix nil sctmlibrt value during migration
    
    * Updating mod version number and changelog
    
    ---------
    
    Co-authored-by: KiwiHawk <59639+KiwiHawk@users.noreply.github.com>

--- migrations/ScienceCostTweaker_1.01.02.lua
+++ migrations/ScienceCostTweaker_1.01.02.lua
@@ -1,3 +1,5 @@
+local sctmlibrt = require("sctmlibrt")
+
 for _, force in pairs(game.forces) do
   if force.recipes['sct-t3-flash-fuel'] and force.recipes['sct-t3-flash-fuel'].enabled then
     sctmlibrt.enable_recipe(force.recipes, 'sct-t3-sulfur-lightsource')
